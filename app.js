befd7ce2e3449630b1d986dcad3dbfd01613bbd4
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2024-01-21 23:14:37 -0500

    Part One Completed

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // Car OO Challenge
+
+class Vehicle {
+  constructor(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+
+  honk() {
+    console.log("Beep!");
+  }
+
+  toString() {
+    const { make, model, year } = this;
+    console.log(`The vehicle is a ${make} ${model} from the year ${year}`);
+  }
+}
+
+let myFirstVehicle = new Vehicle("Toyota", "Supra", 2023);
+
+// console.log(myFirstVehicle.toString());
